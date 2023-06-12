281156362a3f4ad5e5a80771d1096381a2f1aa14
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-06-12 12:08:25 +0900

    jpa react

--- src/main/java/com/example/todo/userapi/controller/UserController.java
+++ src/main/java/com/example/todo/userapi/controller/UserController.java
@@ -2,7 +2,9 @@ package com.example.todo.userapi.controller;
 
 import com.example.todo.exception.DuplicatedEmailException;
 import com.example.todo.exception.NoRegisteredArgumentsException;
+import com.example.todo.userapi.dto.request.LoginRequestDTO;
 import com.example.todo.userapi.dto.request.UserRequestSignUpDTO;
+import com.example.todo.userapi.dto.response.LoginResponseDTO;
 import com.example.todo.userapi.dto.response.UserSignUpResponseDTO;
 import com.example.todo.userapi.service.UserService;
 import lombok.RequiredArgsConstructor;
@@ -48,7 +50,7 @@ public class UserController {
 
         try {
             UserSignUpResponseDTO responseDTO = userService.create(dto);
-            return ResponseEntity.ok().body(dto);
+            return ResponseEntity.ok().body(responseDTO);
         } catch (NoRegisteredArgumentsException e) {
             log.warn("필수 가입정보를 입력해주셔야 합니다.");
             return ResponseEntity.badRequest().body(e.getMessage());
@@ -57,5 +59,19 @@ public class UserController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
 
+    }
+
+    //로그인 요청 처리
+    @PostMapping("/signin")
+    public ResponseEntity<?> signIn(
+        @Validated @RequestBody LoginRequestDTO dto
+    ){
+        try {
+            LoginResponseDTO responseDTO = userService.authenticate(dto);
+            return ResponseEntity.ok().body(responseDTO);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
