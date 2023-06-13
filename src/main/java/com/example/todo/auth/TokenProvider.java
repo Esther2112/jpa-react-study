c917b9b39da5195e6c792d411a0824af83447d6f
Jin Han <120617717+Esther2112@users.noreply.github.com>
2023-06-13 10:54:56 +0900

    jwt

--- src/main/java/com/example/todo/auth/TokenProvider.java
+++ src/main/java/com/example/todo/auth/TokenProvider.java
@@ -1,6 +1,8 @@
 package com.example.todo.auth;
 
+import com.example.todo.userapi.entity.Role;
 import com.example.todo.userapi.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -24,7 +26,6 @@ public class TokenProvider {
     private String SECRET_KEY;
 
     // 토근 생성 메서드
-
     /**
      * Json Web Token을 생성하는 메서드
      * @param userEntity - 토큰의 내용(클레임)에 포함될 유저 정보
@@ -40,7 +41,7 @@ public class TokenProvider {
         // 추가 클레임 정의
         Map<String, Object> claims = new HashMap<>();
         claims.put("email", userEntity.getEmail());
-        claims.put("role", userEntity.getRole());
+        claims.put("role", userEntity.getRole().toString());
 
         // 토큰 생성
         return Jwts.builder()
@@ -51,13 +52,38 @@ public class TokenProvider {
                         , SignatureAlgorithm.HS512
                 )
                 // token payload에 들어갈 클레임 설정
+                .setClaims(claims)
                 .setIssuer("바닐라겅듀") // iss : 발급자 정보, 회사 이름이나 등등을 쓴다
                 .setIssuedAt(new Date()) // iat : 발급 시각, LocalDate 지원 안한다
                 .setExpiration(expiry) // exp : 만료 시각
                 .setSubject(userEntity.getId()) // sub : 토큰을 식별할 수 있는 주요데이터
                 // 여기까지가 JWT를 만들 때 항상 넣는 데이터
-                .setClaims(claims)
                 .compact();
 
+    }
+
+    /**
+     * 클라이언트가 전송한 토큰을 디코딩하여 토큰의 위조여부를 확인
+     * 토큰을 json으로 파싱해서 클레임(토큰정보)를 리턴
+     * @param token
+     * return - 토큰 안에 있는 인증된 유저 정보를 반환
+     */
+    public TokenUserInfo validateAndGetTokenUserInfo(String token) {
+        //해체할떄 쓰는 빌더
+        Claims claims = Jwts.parserBuilder()
+                // 토큰 발급자의 발급 당시의 서명을 넣어줌
+                .setSigningKey(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()))
+                //서명 위조검사: 위조된 경우 예외가 발생
+                // 위조되지 않은 경우 페이로드를 리턴
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+        log.info("claims: {}", claims);
+
+        return TokenUserInfo.builder()
+                .userId(claims.getSubject())
+                .email(claims.get("email", String.class))
+                .role(Role.valueOf(claims.get("role", String.class)))
+                .build();
     }
 }
